291c0881e9aed31b6791f3225763157cff6a1da0
yuzuki <nanami2962@gmail.com>
2022-07-04 19:39:38 +0800

    create win32 window

---
+++ src/renderer/vk_renderer.cpp
@@ -0,0 +1,21 @@
+#include <vulkan/vulkan.h>
+
+struct VkContext
+{
+    VkInstance instance;
+};
+
+bool vk_init(VkContext *vkcontext)
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Pong";
+    appInfo.pEngineName = "Ponggine";
+
+    VkInstanceCreateInfo instanceInfo{};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkResult result = vkCreateInstance(&instanceInfo, nullptr, &vkcontext->instance);
+    return result == VK_SUCCESS;
+}
